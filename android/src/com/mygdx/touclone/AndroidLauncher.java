7a5107470daea3203a82c1daeeb6ca1a36f58683
Tristan Awayan <jawayan1@binghamton.edu>
2021-03-29 10:32:10 -0400

    Initialize libGDX Project
    Start Project for Game 1, to submit Game Sketch in 2 days

---
+++ android/src/com/mygdx/touclone/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.touclone;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.touclone.Touclone;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Touclone(), config);
+	}
+}
