f0f678a55306a1be23e288218affd4e375e64e5f
Tristan Awayan <jawayan1@binghamton.edu>
2021-04-07 09:38:11 -0400

    Game1 Update - Changing Screens
    Game is now portrait rather than landscape
    Screen now changes from title, to game, to end screen via touch inputs

---
+++ core/src/com/mygdx/touclone/TitleScreen.java
@@ -0,0 +1,62 @@
+package com.mygdx.touclone;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+
+public class TitleScreen extends ScreenAdapter {
+
+    Touclone game;
+
+    public TitleScreen(Touclone game){
+        this.game = game;
+    }
+
+    @Override
+    public void show(){
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            @Override
+            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+                System.out.println("screen touched, switching screens");
+                game.setScreen(new GameScreen(game));
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, .5f, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.font.draw(game.batch, "THIS IS TITLE SCREEN!", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .25f);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        Gdx.input.setInputProcessor(null);
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
--- core/src/com/mygdx/touclone/Touclone.java
+++ core/src/com/mygdx/touclone/Touclone.java
@@ -1,33 +1,51 @@
 package com.mygdx.touclone;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 
-public class Touclone extends ApplicationAdapter {
+public class Touclone extends Game {
+
+	// contains all the game resources and input handlers, will get passed from screen to screen
+
 	SpriteBatch batch;
-	Texture img;
-	
+	ShapeRenderer shapeRenderer;
+	BitmapFont font;
+	int w, h;
+
+	class TouchInfo{
+		public float touchX = 0;
+		public float touchY = 0;
+		public boolean touched = false;
+	}
+
+	TouchInfo finger;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shapeRenderer = new ShapeRenderer();
+		font = new BitmapFont(Gdx.files.internal("vcr.fnt"));
+		font.setColor(Color.WHITE);
+		finger = new TouchInfo();
+		setScreen(new TitleScreen(this));
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		shapeRenderer.dispose();
+		font.dispose();
 	}
+
+
 }
